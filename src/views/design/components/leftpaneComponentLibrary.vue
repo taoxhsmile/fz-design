e6fb19c36e2a0c029643d8f1a155783ae49b51b7
陶新华 <taoxinhua@taoxinhuadeMacBook-Pro.local>
2019-09-20 18:11:07 +0800

    优化组件列表

--- src/views/design/components/leftpaneComponentLibrary.vue
+++ src/views/design/components/leftpaneComponentLibrary.vue
@@ -3,38 +3,65 @@
     <el-tabs stretch type="card" v-model="activeName">
       <el-tab-pane label="基础组件" name="basic">
         <ul>
-          <li v-for="(basicPreviewData, i) in basicPreviewDatas" :key="i">
-            <shortcut :basicPreviewData="basicPreviewData">
-              <div class="content">
-                <fz-icon :name="basicPreviewData.componentInfo.icon"></fz-icon>
-                <p>{{ basicPreviewData.componentInfo.name }}</p>
-              </div>
-            </shortcut>
-          </li>
+          <template v-for="(basicPreviewData, i) in basicPreviewDatas">
+            <li class="item" :key="i">
+              <shortcut :basicPreviewData="basicPreviewData">
+                <div class="content">
+                  <fz-icon
+                    :name="basicPreviewData.componentInfo.icon"
+                  ></fz-icon>
+                  <p>{{ basicPreviewData.componentInfo.name }}</p>
+                </div>
+              </shortcut>
+            </li>
+            <li
+              class="line"
+              :key="i + 'line'"
+              v-if="showLine(i, basicPreviewDatas)"
+            ></li>
+          </template>
         </ul>
       </el-tab-pane>
       <el-tab-pane label="布局组件" name="layout">
         <ul>
-          <li v-for="(basicPreviewData, i) in layoutPreviewDatas" :key="i">
-            <shortcut :basicPreviewData="basicPreviewData">
-              <div class="content">
-                <fz-icon :name="basicPreviewData.componentInfo.icon"></fz-icon>
-                <p>{{ basicPreviewData.componentInfo.name }}</p>
-              </div>
-            </shortcut>
-          </li>
+          <template v-for="(basicPreviewData, i) in layoutPreviewDatas">
+            <li class="item" :key="i">
+              <shortcut :basicPreviewData="basicPreviewData">
+                <div class="content">
+                  <fz-icon
+                    :name="basicPreviewData.componentInfo.icon"
+                  ></fz-icon>
+                  <p>{{ basicPreviewData.componentInfo.name }}</p>
+                </div>
+              </shortcut>
+            </li>
+            <li
+              class="line"
+              :key="i + 'line'"
+              v-if="showLine(i, layoutPreviewDatas)"
+            ></li>
+          </template>
         </ul>
       </el-tab-pane>
       <el-tab-pane label="功能组件" name="functional">
         <ul>
-          <li v-for="(basicPreviewData, i) in functionalPreviewDatas" :key="i">
-            <shortcut :basicPreviewData="basicPreviewData">
-              <div class="content">
-                <fz-icon :name="basicPreviewData.componentInfo.icon"></fz-icon>
-                <p>{{ basicPreviewData.componentInfo.name }}</p>
-              </div>
-            </shortcut>
-          </li>
+          <template v-for="(basicPreviewData, i) in functionalPreviewDatas">
+            <li class="item" :key="i">
+              <shortcut :basicPreviewData="basicPreviewData">
+                <div class="content">
+                  <fz-icon
+                    :name="basicPreviewData.componentInfo.icon"
+                  ></fz-icon>
+                  <p>{{ basicPreviewData.componentInfo.name }}</p>
+                </div>
+              </shortcut>
+            </li>
+            <li
+              class="line"
+              :key="i + 'line'"
+              v-if="showLine(i, functionalPreviewDatas)"
+            ></li>
+          </template>
         </ul>
       </el-tab-pane>
     </el-tabs>
@@ -54,6 +81,12 @@ export default {
       layoutPreviewDatas: getPreviewDataByKind("layout"),
       functionalPreviewDatas: getPreviewDataByKind("functional")
     };
+  },
+  methods: {
+    showLine(i, data) {
+      let len = data.length;
+      return (i + 1) % 3 === 0 && i < len - 1;
+    }
   }
 };
 </script>
@@ -75,20 +108,28 @@ export default {
 
   ul {
     display: flex;
+    flex-wrap: wrap;
     li {
-      width: 100px;
-      height: 100px;
-      text-align: center;
-      box-sizing: border-box;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      cursor: pointer;
-      i {
-        font-size: 22px;
+      &.item {
+        width: 100px;
+        height: 100px;
+        text-align: center;
+        box-sizing: border-box;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        cursor: pointer;
+        i {
+          font-size: 22px;
+        }
+        p {
+          margin-top: 8px;
+        }
       }
-      p {
-        margin-top: 8px;
+      &.line {
+        width: 100%;
+        height: 1px;
+        background: #dbdfe6;
       }
     }
   }
